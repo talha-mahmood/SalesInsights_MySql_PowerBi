1936b49b86de4f6ba6935dc0037e4ec17e36bc6b
talha-mahmood <talhamahmood1261@gmail.com>
2024-02-29 00:20:42 +0500

    2

--- db_dump3.sql
+++ db_dump3.sql
@@ -1,1 +1,3 @@
 select sales.transactions.*,sales.date.* from sales.transactions inner join sales.date on sales.transactions.order_date=sales.date.date where sales.date.year=2020;
+
+select sum(sales.transactions.sales_amount) from sales.transactions inner join sales.date on sales.transactions.order_date=sales.date.date where sales.date.year=2020;
